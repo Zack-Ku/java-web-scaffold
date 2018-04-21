df782b2dc40d450edb2f212bb5faff19d742bb5c
Zack <gza_scnu@163.com>
2018-04-21 13:13:48 +0800

    优化greeting demo

--- service/src/main/java/com/zackku/service/helllo/service/GreetingService.java
+++ service/src/main/java/com/zackku/service/helllo/service/GreetingService.java
@@ -5,8 +5,6 @@ import com.zackku.service.helllo.mapper.GreetingMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
  * @author Zack
  * @date 2018/4/21
@@ -14,7 +12,6 @@ import java.util.concurrent.atomic.AtomicLong;
 @Service
 public class GreetingService {
     private static final String TEMPLATE = "this is %s!";
-    private final AtomicLong counter = new AtomicLong();
 
     private GreetingMapper greetingMapper;
 
@@ -25,7 +22,6 @@ public class GreetingService {
 
     public Greeting createGreet(String name) {
         Greeting greeting = new Greeting();
-        greeting.setId(counter.incrementAndGet());
         greeting.setContent(String.format(TEMPLATE, name));
         greetingMapper.insert(greeting);
         return greeting;
